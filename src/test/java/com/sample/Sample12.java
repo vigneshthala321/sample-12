6ee719b9edc6dd86bbce76eb1b3ba693436957b0
vigneshthala321 <vigneshthala321@gmail.com>
2022-11-04 21:11:46 +0530

    first master

---
+++ src/test/java/com/sample/Sample12.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+public class Sample12 {
+public static void main(String[] args) {
+	System.out.println("vicky");
+	System.out.println("dhana");
+	System.out.println("nakul");
+	
+	
+	
+}
+}
