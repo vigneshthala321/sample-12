3da6fd8997b2afb0461e0daddd8f7fdf11191bae
vigneshthala321 <vigneshthala321@gmail.com>
2022-11-04 21:20:28 +0530

    vicky second

--- src/test/java/com/sample/Sample12.java
+++ src/test/java/com/sample/Sample12.java
@@ -5,6 +5,9 @@ public static void main(String[] args) {
 	System.out.println("vicky");
 	System.out.println("dhana");
 	System.out.println("nakul");
+	System.out.println("partha");
+	System.out.println("prakash");
+	System.out.println("ragu");
 	
 	
 	
